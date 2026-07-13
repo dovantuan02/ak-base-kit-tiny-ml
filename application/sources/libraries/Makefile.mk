69cc9bd64b35bed6b638cad650d6e19884165874
tuandv60 <tuandv60@fpt.com>
2026-07-13 09:05:55 +0700

    [ADD] update submodule

--- application/sources/libraries/Makefile.mk
+++ application/sources/libraries/Makefile.mk
@@ -1,6 +1,21 @@
 include sources/libraries/QRCode/Makefile.mk
 include sources/libraries/ArduinoJson/Makefile.mk
 include sources/libraries/nlohmann/Makefile.mk
-include sources/libraries/SparkFun_ICM-20948_ArduinoLibrary/Makefile.mk
-include sources/libraries/emlearn/Makefile.mk
-# include sources/libraries/tflite-micro/Makefile.mk
+
+# Submodules ICM-20948
+CFLAGS += -I./sources/libraries/SparkFun_ICM-20948_ArduinoLibrary/src
+CPPFLAGS += -I./sources/libraries/SparkFun_ICM-20948_ArduinoLibrary/src
+
+CFLAGS += -I./sources/libraries/SparkFun_ICM-20948_ArduinoLibrary/src/util
+CPPFLAGS += -I./sources/libraries/SparkFun_ICM-20948_ArduinoLibrary/src/util
+
+VPATH += sources/libraries/SparkFun_ICM-20948_ArduinoLibrary/src
+VPATH += sources/libraries/SparkFun_ICM-20948_ArduinoLibrary/src/util
+
+SOURCES 	+= sources/libraries/SparkFun_ICM-20948_ArduinoLibrary/src/util/ICM_20948_C.c
+SOURCES_CPP += sources/libraries/SparkFun_ICM-20948_ArduinoLibrary/src/ICM_20948.cpp
+
+#Submodules emlearn
+CFLAGS += -I./sources/libraries/emlearn/emlearn
+CPPFLAGS += -I./sources/libraries/emlearn/emlearn
+
